99cb196bb09f2d296f847413828f5b91b0e2274e
노경필 <kpnoh@neobay.co.kr>
2023-02-02 14:40:29 +0900

    custom ApiResponse, beanUtil 추가

---
+++ src/main/java/com/nkp/myblog/util/BeanUtils.java
@@ -0,0 +1,23 @@
+package com.nkp.myblog.util;
+
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanUtils implements ApplicationContextAware {
+
+    @Getter
+    private static ApplicationContext context;
+
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> beanClass) {
+        return context.getBean(beanClass);
+    }
+}
