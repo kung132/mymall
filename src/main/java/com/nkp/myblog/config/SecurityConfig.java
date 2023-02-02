a8a2ff62b8fa58e8876b70f1880c0607eedc0855
노경필 <kpnoh@neobay.co.kr>
2023-02-02 16:37:08 +0900

    security, cors config 생성

--- src/main/java/com/nkp/myblog/config/SecurityConfig.java
+++ src/main/java/com/nkp/myblog/config/SecurityConfig.java
@@ -1,4 +1,39 @@
 package com.nkp.myblog.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final CorsConfig corsConfig;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .addFilter(corsConfig.corsFilter())
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
 }
